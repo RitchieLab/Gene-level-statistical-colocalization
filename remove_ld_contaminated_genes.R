3c5a52220f9e927ab73354bc0423a8680d75185c
sudhaveturi <sudhaveturi@gmail.com>
2020-04-24 21:43:50 -0400

    Update remove_ld_contaminated_genes.R

--- remove_ld_contaminated_genes.R
+++ remove_ld_contaminated_genes.R
@@ -20,8 +20,8 @@ suppressMessages(library(data.table))
 
 opt_list <- list(
   make_option("--file_name",type="character",help="Load coloc output file obtained after running run_gcta_and_coloc.R"),
-  make_option("--coloc_p_h3_threshold",type="numeric",default=0,5,help="Threshold for minP[H3] peer gene across all lead SNPs in the gene"),
-  make_option("--coloc_p_h4_threshold",type="numeric",default=0.5,help="Threshold for maxP[H4] per gene across all lead SNPs in the gene"),
+  make_option("--coloc_p_h3_threshold",type="numeric",default=0,5,help="Threshold for minP[H3] per gene across all lead SNPs in the gene (default=%default)"),
+  make_option("--coloc_p_h4_threshold",type="numeric",default=0.5,help="Threshold for maxP[H4] per gene across all lead SNPs in the gene (default=%default)"),
   make_option("--output_folder",type="character",help="Folder where list of genes after removing LD contaminated ones is saved")
 )
 
@@ -42,4 +42,5 @@ h4 = data[data[, .I[Coloc.H4 == max(Coloc.H4)], by=Gene]$V1]
 gene.list.h3 = h3[which(h3[,"Coloc.H3"]<=coloc.p.h3.threshold),"Gene"]
 gene.list.h4 = h4[which(h4[,"Coloc.H4"]>=coloc.p.h4.threshold),"Gene"]
 gene.list = intersect(as.data.frame(gene.list.h3),as.data.frame(gene.list.h4))
-if(length(gene.list)==0) stop("No genes that pass the LD contamination filter") else write.table(paste0(output.folder,"/Final_list_of_genes_no_ld_contamination.txt"),row.names=F,col.names=F,sep="\t",quote=F)
+if(length(gene.list)==0) stop("No genes that pass the LD contamination filter") 
+else write.table(paste0(output.folder,"/Final_list_of_genes_no_ld_contamination.txt"),row.names=F,col.names=F,sep="\t",quote=F)
